1e38d2040b058ac9f074dc44320cfb320aab6744
Eric Rochester <erochest@virginia.edu>
2014-03-28 10:38:44 -0400

    Removed broken tasty-hspec support.

--- specs/Specs.hs
+++ specs/Specs.hs
@@ -7,11 +7,8 @@ import Test.Tasty
 -- import Test.Tasty.QuickCheck as QC
 -- import Test.Tasty.HUnit as HU
 -- import Test.Tasty.Golden as TG
-import Test.Tasty.Hspec
+-- import Test.Tasty.Hspec
 
-
-specs :: Spec
-specs = undefined
 
 tests :: TestTree
 tests = testGroup "cabal-new"
